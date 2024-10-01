266306284884ce128e35087ab6937c3dffe3b77e
Satvik <sharmasatvik43@gmail.com>
2024-10-01 21:09:43 +0530

    Box Animation Done

---
+++ 01_Basics/script.js
@@ -0,0 +1,26 @@
+// gsap.to()        // Initial to final
+// gsap.from()      // Final to Initial
+
+// What we want to animate as a key-value pair
+
+gsap.to("#box1", {
+    x: 1000,
+    duration: 2,
+    delay: 1, 
+    rotate: 360, 
+    backgroundColor: 'aqua',
+    borderRadius: "50%",
+    repeat: -1,
+    yoyo: true
+})  
+
+gsap.from("#box2", {
+    x: 1000,
+    duration: 2,
+    delay: 1,
+    rotate: 360,
+    backgroundColor: 'green',
+    borderRadius: "50%",
+    repeat: -1,
+    ease: "bounce.out"
+})
